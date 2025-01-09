2f4673fadc4fe5495821ed33c03e23aa91450a72
lordsayur <umar_317@hotmail.com>
2025-01-09 13:57:55 +0000

    ✨ core: update Game.start method to accept an object with board states and current state, and add corresponding tests

--- packages/core/src/game.test.ts
+++ packages/core/src/game.test.ts
@@ -3,45 +3,68 @@ import { BoardState, Game } from "./game";
 
 describe("Game", () => {
   describe("start", () => {
-    it("should initialize board state with '-' by default", () => {
-      // arrange
-      const game = new Game();
+    describe("without board states", () => {
+      it("should initialize board state with '-' by default", () => {
+        // arrange
+        const game = new Game();
 
-      // act
-      game.start();
+        // act
+        game.start();
 
-      // assert
-      expect(game.boardStates).toHaveLength(1);
-      expect(game.currentBoardState).toStrictEqual<BoardState>([
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-      ]);
+        // assert
+        expect(game.boardStates).toHaveLength(1);
+        expect(game.currentBoardState).toStrictEqual<BoardState>([
+          ["-", "-", "-"],
+          ["-", "-", "-"],
+          ["-", "-", "-"],
+        ]);
+      });
     });
 
-    it("should initialize board state with specified state", () => {
-      // arrange
-      const game = new Game();
+    describe("with board states", () => {
+      it("should initialize board state with specified state", () => {
+        // arrange
+        const game = new Game();
 
-      // act
-      game.start({
-        initialState: [
-          [
-            ["x", "-", "-"],
-            ["-", "-", "-"],
-            ["-", "-", "-"],
+        // act
+        game.start({
+          states: [
+            [
+              ["x", "-", "-"],
+              ["-", "-", "-"],
+              ["-", "-", "-"],
+            ],
           ],
-        ],
-        currentBoardState: 0,
+          currentState: 0,
+        });
+
+        // assert
+        expect(game.boardStates).toHaveLength(1);
+        expect(game.currentBoardState).toStrictEqual<BoardState>([
+          ["x", "-", "-"],
+          ["-", "-", "-"],
+          ["-", "-", "-"],
+        ]);
       });
 
-      // assert
-      expect(game.boardStates).toHaveLength(1);
-      expect(game.currentBoardState).toStrictEqual<BoardState>([
-        ["x", "-", "-"],
-        ["-", "-", "-"],
-        ["-", "-", "-"],
-      ]);
+      it("should throw invalid current state exception", () => {
+        // arrange
+        const game = new Game();
+
+        // act
+        expect(() =>
+          game.start({
+            states: [
+              [
+                ["x", "-", "-"],
+                ["-", "-", "-"],
+                ["-", "-", "-"],
+              ],
+            ],
+            currentState: 1,
+          }),
+        ).toThrowError("current board state is out of range");
+      });
     });
   });
 });
--- packages/core/src/game.ts
+++ packages/core/src/game.ts
@@ -6,13 +6,13 @@ export class Game {
   private _currentBoardState: number = 0;
   private _boardStates: BoardState[] = [];
 
-  start(boardState?: {
-    initialState: BoardState[];
-    currentBoardState: number;
-  }) {
-    if (boardState) {
-      this._boardStates = boardState.initialState;
-      this._currentBoardState = boardState.currentBoardState;
+  start(boardStates?: { states: BoardState[]; currentState: number }) {
+    if (boardStates) {
+      if (boardStates.currentState + 1 > boardStates.states.length) {
+        throw new Error("current board state is out of range");
+      }
+      this._boardStates = boardStates.states;
+      this._currentBoardState = boardStates.currentState;
       return;
     }
 
